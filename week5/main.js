ac667db9cbf37017429757834de5cc6c54710fa9
Roman Nikko Albania <rbalbania@student.apc.edu.ph>
2024-01-15 21:20:09 +0800

    updated with 3 js

--- week5/main.js
+++ week5/main.js
@@ -7,11 +7,4 @@ function contact() {
     window.open(`mailto:${myEmail}?subject=from ${name} (${email})&body=${message}`);
 } 
 
-// Get the cursor element
-const cursor = document.querySelector(".cursor");
 
-// Update its position on mousemove
-window.addEventListener("mousemove", (event) => {
-  cursor.style.left = event.clientX + "px";
-  cursor.style.top = event.clientY + "px";
-});
