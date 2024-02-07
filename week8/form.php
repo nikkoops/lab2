b1848063168d1288a35dda6770e0ea9c4c2255a0
nikkoops <126082666+nikkoops@users.noreply.github.com>
2024-02-07 19:47:53 +0800

    Update form.php

--- week8/form.php
+++ week8/form.php
@@ -102,9 +102,9 @@ echo $gender;
 
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
+$username = "webprogmi222_sf221";
+$password = "xE*Y2nleNVvZm[!!  ";
+$dbname = "webprogmi222_sf221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -113,7 +113,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO MyGuests (name, email, website, comment, gender) 
+$sql = "INSERT INTO rbalbania_myguests (name, email, website, comment, gender) 
 VALUES ('$name', '$email', '$website', '$comment', '$gender')";
 
 
